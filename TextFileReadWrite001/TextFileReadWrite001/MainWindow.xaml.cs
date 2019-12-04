b7d5c09176fbdbb1685ef8cae9f08d5ddc6d54f4
Jean-Marc Flamand <7jtf@videotron.ca>
2019-12-04 08:29:50 -0500

    Add file Browser and Read data
    Able to read data in random column

--- TextFileReadWrite001/TextFileReadWrite001/MainWindow.xaml.cs
+++ TextFileReadWrite001/TextFileReadWrite001/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TextFileChallenge;
 
 namespace TextFileReadWrite001
 {
@@ -23,6 +26,62 @@ namespace TextFileReadWrite001
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string myFile;
+                string myPath;
+
+                //  myFile = openFileDialog.SafeFileName; //Only the file name
+                myFile = openFileDialog.FileName; //Only the file name
+                myPath = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+                //myPath = myPath.Replace(@"\\", @"\");
+                //MessageBox.Show (myPath);
+
+                readtextfile(myFile);
+            }
+        }
+
+        public void readtextfile(string thepathfile)
+        {
+            int FirstNameIndex;
+            int LastNameIndex;
+            int AgeIndex;
+            int IsAliveIndex;
+            
+            List<UserModel> users = new List<UserModel>();
+
+            var reader = new StreamReader(thepathfile);
+            List<string> headers = new List<string>();
+            headers = reader.ReadLine().Split(',').ToList(); //Read the first line (header line) and put in a list
+
+            // Get The Index of each header in case of not in correct order
+            FirstNameIndex = headers.FindIndex(headers => headers.StartsWith("FirstName"));
+            LastNameIndex = headers.FindIndex(headers => headers.StartsWith("LastName"));
+            AgeIndex = headers.FindIndex(headers => headers.StartsWith("Age"));
+            IsAliveIndex = headers.FindIndex(headers => headers.StartsWith("IsAlive"));
+
+
+            while (!reader.EndOfStream)
+            {
+                var values = reader.ReadLine().Split(',');
+                UserModel newUser = new UserModel();
+
+                newUser.FirstName = values[FirstNameIndex];
+                newUser.LastName = values[LastNameIndex];
+                newUser.Age = int.Parse(values[AgeIndex]);
+                newUser.IsAlive = bool.Parse(values[IsAliveIndex]);
+                // if 0 or 1 use newUser.IsAlive = Convert.ToBoolean(Convert.ToInt32(values[3]));
+                MessageBox.Show(newUser.DisplayText);
+                //MessageBox.Show(string.Format("{0} = {1};", headers[i], values[i]));
+                FileLstB.Items.Add(newUser.DisplayText);
+            }
+
         }
     }
 }
